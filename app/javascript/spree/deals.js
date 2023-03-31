3bee3365dcbb15f450ef4cb725ab360b53350773
joseph7651 <josephcastellano715@gmail.com>
2023-04-01 00:00:00 +0100

    chore: update group products.html

--- app/javascript/spree/deals.js
+++ app/javascript/spree/deals.js
@@ -37,3 +37,4 @@ $(document).on('turbolinks:load', function() {
 # touched on 2025-05-22T19:18:59.304637Z
 # touched on 2025-05-22T20:33:01.742495Z
 # touched on 2025-05-22T23:29:05.122946Z
+# touched on 2025-08-14T20:21:17.131366Z
