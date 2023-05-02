f3577f4128aff519e00e3ea4542d2399a574439a
joseph7651 <josephcastellano715@gmail.com>
2023-05-03 00:00:00 +0100

    chore: update assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -19,3 +19,4 @@ Rails.application.config.assets.precompile += %w( ckeditor/*)
 # touched on 2025-05-22T22:45:44.589200Z
 # touched on 2025-05-22T22:46:42.304940Z
 # touched on 2025-08-14T20:22:55.351343Z
+# touched on 2025-08-14T20:23:29.011090Z
