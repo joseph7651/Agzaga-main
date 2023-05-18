c3b4f9f83c7d6c4296aed7a43fe740e6ec7f3580
joseph7651 <josephcastellano715@gmail.com>
2023-05-19 00:00:00 +0100

    chore: update mill latest posts.html

--- app/javascript/spree/chuckwagon_dvd_product.js
+++ app/javascript/spree/chuckwagon_dvd_product.js
@@ -179,3 +179,4 @@ $(document).on('turbolinks:load', function() {
 # touched on 2025-05-22T20:44:38.675613Z
 # touched on 2025-05-22T22:38:59.651104Z
 # touched on 2025-05-22T23:43:48.643702Z
+# touched on 2025-08-14T20:24:09.347687Z
