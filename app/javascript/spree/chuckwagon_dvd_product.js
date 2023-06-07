9b130e347b91548eac34c9189638193e08de36bd
joseph7651 <josephcastellano715@gmail.com>
2023-06-08 00:00:00 +0100

    chore: update google ads.html

--- app/javascript/spree/chuckwagon_dvd_product.js
+++ app/javascript/spree/chuckwagon_dvd_product.js
@@ -180,3 +180,4 @@ $(document).on('turbolinks:load', function() {
 # touched on 2025-05-22T22:38:59.651104Z
 # touched on 2025-05-22T23:43:48.643702Z
 # touched on 2025-08-14T20:24:09.347687Z
+# touched on 2025-08-14T20:25:07.968837Z
