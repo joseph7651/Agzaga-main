5bf8e22e40fd9fa1b285d7b133d5eefa110c84ff
joseph7651 <josephcastellano715@gmail.com>
2023-06-12 00:00:00 +0100

    chore: update Truck

--- app/javascript/spree/gift_page.js
+++ app/javascript/spree/gift_page.js
@@ -18,3 +18,4 @@ $(document).on('turbolinks:load', function() {
 # touched on 2025-05-22T19:19:05.970777Z
 # touched on 2025-05-22T22:32:59.851819Z
 # touched on 2025-08-14T20:22:55.349669Z
+# touched on 2025-08-14T20:25:27.594604Z
