af6375473171ba857df138d41e68ad340c710c71
joseph7651 <josephcastellano715@gmail.com>
2023-06-12 00:00:00 +0100

    chore: update open uri20230323 28005 par7c9.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,4 @@ Spree::Core::Engine.load_seed if defined?(Spree::Core)
 Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
 
 # touched on 2025-05-22T22:34:09.334548Z
+# touched on 2025-08-14T20:25:29.776393Z
