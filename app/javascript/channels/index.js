ef0e1736f43b2b65d0253ce2e03551abd7f9cbc1
joseph7651 <josephcastellano715@gmail.com>
2023-06-18 00:00:00 +0100

    chore: update grey dollar icon

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -8,3 +8,4 @@ channels.keys().forEach(channels)
 # touched on 2025-05-22T20:44:16.566234Z
 # touched on 2025-05-22T22:43:21.059036Z
 # touched on 2025-08-14T20:18:32.662744Z
+# touched on 2025-08-14T20:25:44.860895Z
