a1273e4491aebe3c3a47473316084f21c9869640
joseph7651 <josephcastellano715@gmail.com>
2023-07-18 00:00:00 +0100

    chore: update mill by category.html

--- app/javascript/spree/product_variants.js
+++ app/javascript/spree/product_variants.js
@@ -272,3 +272,4 @@ $(document).ready(function() {
 # touched on 2025-05-22T21:58:06.033910Z
 # touched on 2025-05-22T22:58:53.958315Z
 # touched on 2025-08-14T20:22:14.495193Z
+# touched on 2025-08-14T20:27:26.190842Z
