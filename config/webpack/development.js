0bf08fca3ccb9245de59745b4ffa42f71ee48ee3
joseph7651 <josephcastellano715@gmail.com>
2023-10-30 00:00:00 +0000

    chore: update error page bg mob

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -8,3 +8,4 @@ module.exports = environment.toWebpackConfig()
 # touched on 2025-05-22T23:39:17.607571Z
 # touched on 2025-08-14T20:20:50.171117Z
 # touched on 2025-08-14T20:30:03.378927Z
+# touched on 2025-08-14T20:31:38.065249Z
