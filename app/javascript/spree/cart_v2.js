c664c6bd1df69f6d495d180ed09e695a20d1352b
joseph7651 <josephcastellano715@gmail.com>
2024-02-03 00:00:00 +0000

    chore: update generic deals tab

--- app/javascript/spree/cart_v2.js
+++ app/javascript/spree/cart_v2.js
@@ -112,3 +112,4 @@ $(document).on('turbolinks:load', function() {
 # touched on 2025-05-22T22:47:16.879248Z
 # touched on 2025-05-22T23:01:55.564165Z
 # touched on 2025-08-14T20:33:15.750215Z
+# touched on 2025-08-14T20:36:02.993452Z
