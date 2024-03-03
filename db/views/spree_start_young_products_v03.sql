643301bd5627196d5104237f221afaebacb7d3c1
joseph7651 <josephcastellano715@gmail.com>
2024-03-03 00:00:00 +0000

    chore: update spree start young products v03

--- db/views/spree_start_young_products_v03.sql
+++ db/views/spree_start_young_products_v03.sql
@@ -12,3 +12,4 @@ ORDER BY "spree_labels_products"."position" ASC
 # touched on 2025-05-22T23:28:46.655959Z
 # touched on 2025-05-22T23:39:12.526760Z
 # touched on 2025-08-14T20:30:48.544772Z
+# touched on 2025-08-14T20:37:26.119667Z
