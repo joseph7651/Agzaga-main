b88e8c1afe3bd6b082cac2b40649732c3db3884d
joseph7651 <josephcastellano715@gmail.com>
2024-05-02 00:00:00 +0100

    chore: update tote bag ruby

--- app/javascript/spree/cart_v2.js
+++ app/javascript/spree/cart_v2.js
@@ -114,3 +114,4 @@ $(document).on('turbolinks:load', function() {
 # touched on 2025-08-14T20:33:15.750215Z
 # touched on 2025-08-14T20:36:02.993452Z
 # touched on 2025-08-14T20:37:53.549135Z
+# touched on 2025-08-14T20:40:26.418525Z
