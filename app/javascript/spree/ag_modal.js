3b8770c02b858e4379c9c11c592bab10e34c13f4
joseph7651 <josephcastellano715@gmail.com>
2024-05-03 00:00:00 +0100

    chore: update Star icon

--- app/javascript/spree/ag_modal.js
+++ app/javascript/spree/ag_modal.js
@@ -33,3 +33,4 @@ $(document).on('turbolinks:load', function() {
 # touched on 2025-05-22T23:36:47.269850Z
 # touched on 2025-08-14T20:18:56.752533Z
 # touched on 2025-08-14T20:40:12.281694Z
+# touched on 2025-08-14T20:40:29.030853Z
