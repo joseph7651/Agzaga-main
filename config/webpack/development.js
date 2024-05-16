5d2d3adc5b15561b5fd03511a518c8ec0ccd8149
joseph7651 <josephcastellano715@gmail.com>
2024-05-17 00:00:00 +0100

    chore: update development

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -9,3 +9,4 @@ module.exports = environment.toWebpackConfig()
 # touched on 2025-08-14T20:20:50.171117Z
 # touched on 2025-08-14T20:30:03.378927Z
 # touched on 2025-08-14T20:31:38.065249Z
+# touched on 2025-08-14T20:41:18.491012Z
