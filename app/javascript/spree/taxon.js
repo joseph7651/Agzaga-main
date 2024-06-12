91bd583b46b3a78831642240b5273c561e1edaed
joseph7651 <josephcastellano715@gmail.com>
2024-06-13 00:00:00 +0100

    chore: update solidus long back white

--- app/javascript/spree/taxon.js
+++ app/javascript/spree/taxon.js
@@ -13,3 +13,4 @@ $(document).on('turbolinks:load', function() {
 # touched on 2025-05-22T23:30:03.280412Z
 # touched on 2025-05-22T23:43:57.443933Z
 # touched on 2025-08-14T20:40:36.982004Z
+# touched on 2025-08-14T20:42:47.621351Z
