ed77966ac7a311875dc255ee55b16f726bf3dd11
joseph7651 <josephcastellano715@gmail.com>
2024-07-05 00:00:00 +0100

    chore: update black friday mobile

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -9,3 +9,4 @@ module.exports = environment.toWebpackConfig()
 # touched on 2025-08-14T20:31:15.109754Z
 # touched on 2025-08-14T20:38:16.281985Z
 # touched on 2025-08-14T20:41:41.773669Z
+# touched on 2025-08-14T20:43:33.600127Z
