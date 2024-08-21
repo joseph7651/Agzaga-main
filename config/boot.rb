c7cf5f737730125cae7a54d6fc1d188e1315932b
joseph7651 <josephcastellano715@gmail.com>
2024-08-22 00:00:00 +0100

    chore: update boot

--- config/boot.rb
+++ config/boot.rb
@@ -5,3 +5,4 @@ require "bootsnap/setup" # Speed up boot time by caching expensive operations.
 
 # touched on 2025-05-22T23:24:15.599934Z
 # touched on 2025-08-14T20:45:15.864159Z
+# touched on 2025-08-14T20:45:31.594707Z
