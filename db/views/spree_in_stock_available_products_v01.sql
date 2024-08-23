0a523db7f2dbfd47c8954a9459964e7d93b90ea1
joseph7651 <josephcastellano715@gmail.com>
2024-08-24 00:00:00 +0100

    chore: update spree make easy products v01

--- db/views/spree_in_stock_available_products_v01.sql
+++ db/views/spree_in_stock_available_products_v01.sql
@@ -11,3 +11,4 @@ WHERE "spree_products"."deleted_at" IS NULL AND ("spree_products".available_on <
 # touched on 2025-08-14T20:32:55.880230Z
 # touched on 2025-08-14T20:39:36.266449Z
 # touched on 2025-08-14T20:43:03.882067Z
+# touched on 2025-08-14T20:45:35.762030Z
