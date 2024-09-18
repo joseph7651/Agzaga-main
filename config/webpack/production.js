7d6eead5dade1827a8d59934472529324055b638
joseph7651 <josephcastellano715@gmail.com>
2024-09-19 00:00:00 +0100

    chore: update tracking lookups controller

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -8,3 +8,4 @@ module.exports = environment.toWebpackConfig()
 # touched on 2025-05-22T19:19:05.972883Z
 # touched on 2025-05-22T22:32:39.211648Z
 # touched on 2025-08-14T20:32:19.174202Z
+# touched on 2025-08-14T20:46:56.979723Z
