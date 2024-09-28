5bf05525d76b931d5dadb631beb29fad62c76a1d
joseph7651 <josephcastellano715@gmail.com>
2024-09-29 00:00:00 +0100

    chore: update taxons.css

--- app/javascript/spree/checkout_form_v2.js
+++ app/javascript/spree/checkout_form_v2.js
@@ -637,3 +637,4 @@ function checkout_js(event) {
 # touched on 2025-05-22T22:37:11.360764Z
 # touched on 2025-05-22T23:27:41.065143Z
 # touched on 2025-05-22T23:29:33.876129Z
+# touched on 2025-08-14T20:47:34.042698Z
