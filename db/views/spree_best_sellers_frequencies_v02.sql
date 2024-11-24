04ff8eae826ee83799bfcfb5791ace1890cc2725
joseph7651 <josephcastellano715@gmail.com>
2024-11-24 00:00:00 +0000

    chore: update Icon

--- db/views/spree_best_sellers_frequencies_v02.sql
+++ db/views/spree_best_sellers_frequencies_v02.sql
@@ -23,3 +23,4 @@ ORDER BY orders_count DESC limit 50;
 # touched on 2025-08-14T20:22:36.713351Z
 # touched on 2025-08-14T20:33:45.915384Z
 # touched on 2025-08-14T20:36:27.354777Z
+# touched on 2025-08-14T20:50:04.969741Z
